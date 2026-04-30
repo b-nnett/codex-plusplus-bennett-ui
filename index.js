8951904ab663b05ca35a60b1a4675dd7eac4c1d6
bendsp <benjy.desprets@gmail.com>
2026-04-30 17:17:58 +0200

    update: use exact bootstrap brush icon

--- index.js
+++ index.js
@@ -2213,10 +2213,10 @@ const FEATURES = {
 
     const projectColorIcon = () => {
       const icon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      icon.setAttribute("width", "20");
-      icon.setAttribute("height", "20");
-      icon.setAttribute("viewBox", "0 0 20 20");
-      icon.setAttribute("fill", "none");
+      icon.setAttribute("width", "16");
+      icon.setAttribute("height", "16");
+      icon.setAttribute("viewBox", "0 0 16 16");
+      icon.setAttribute("fill", "currentColor");
       icon.setAttribute("aria-hidden", "true");
       icon.setAttribute(MENU_ATTR, "trigger-icon");
       icon.classList.add("icon-sm", "shrink-0");
@@ -2224,28 +2224,10 @@ const FEATURES = {
       const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
       path.setAttribute(
         "d",
-        "M10 3.1C6.1 3.1 3 5.9 3 9.4c0 2.9 2.4 5.2 5.3 5.2h.8c.8 0 1.3.5 1.3 1.1 0 .7.5 1.2 1.3 1.2 3 0 5.3-3.1 5.3-6.7 0-3.9-3.1-7.1-7-7.1Z",
+        "M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.1 6.1 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.1 8.1 0 0 1-3.078.132 4 4 0 0 1-.562-.135 1.4 1.4 0 0 1-.466-.247.7.7 0 0 1-.204-.288.62.62 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896q.19.012.348.048c.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04M4.705 11.912a1.2 1.2 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.4 3.4 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3 3 0 0 0 .126-.75zm1.44.026c.12-.04.277-.1.458-.183a5.1 5.1 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005zm3.582-3.043.002.001h-.002z",
       );
-      path.setAttribute("stroke", "currentColor");
-      path.setAttribute("stroke-width", "1.45");
-      path.setAttribute("stroke-linecap", "round");
-      path.setAttribute("stroke-linejoin", "round");
-
-      const dots = [
-        ["7.2", "8"],
-        ["10", "6.8"],
-        ["12.8", "8"],
-        ["7.9", "11"],
-      ];
+      path.setAttribute("fill", "currentColor");
       icon.appendChild(path);
-      for (const [cx, cy] of dots) {
-        const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        dot.setAttribute("cx", cx);
-        dot.setAttribute("cy", cy);
-        dot.setAttribute("r", "0.65");
-        dot.setAttribute("fill", "currentColor");
-        icon.appendChild(dot);
-      }
       return icon;
     };
 
